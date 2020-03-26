4ec0a59719f0324c1d4d0de041c7db1010425a75
Alejandra Ochoa <ale.ochoafi@gmail.com>
2020-03-26 11:02:42 -0600

    limpiando appjs

--- backend/app.js
+++ backend/app.js
@@ -64,37 +64,7 @@ app.use('/', require('./routes/auth'));
 app.use('/contrato', require('./routes/contratos'));
 app.use('/cliente', require('./routes/cliente'));
 
-/*app.get('/', function (req, res) {
-   
-  var sql = require("mssql");
 
-  // config for your database
-  var config = {
-      user: 'Ale',
-      password: '123',
-      server: 'localhost', 
-      database: 'PortalCliente' 
-  };
-
-  // connect to your database
-  sql.connect(config, function (err) {
-  
-      if (err) console.log(err);
-
-      // create Request object
-      var request = new sql.Request();
-         
-      // query to the database and get the records
-      request.query('select * from Empresa', function (err, recordset) {
-          
-          if (err) console.log(err)
-
-          // send records as a response
-          res.send(recordset);
-          
-      });
-  });
-});*/
 // Uncomment this line for production
 // app.get('/*', (req, res) => res.sendFile(__dirname + '/public/index.html'));
 

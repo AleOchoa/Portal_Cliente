3d3134f4ce7052fbf56943edcb6caf4b951b7914
Alejandra Ochoa <ale.ochoafi@gmail.com>
2020-03-26 20:04:25 -0600

    primera versión consulta de info cliente

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const sql= require('mssql')
 
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
   const request = new sql.Request();
-  request.query('select * from Empresa', function (err, recordset) {
+  request.query('select * from Clientes a left outer join Contrato b on a.IdCliente=b.IdCliente', function (err, recordset) {
             
     if (err) console.log(err)
 
@@ -13,6 +13,21 @@ router.get('/', (req, res, next) => {
     
   });
 //  res.status(200).json({ msg: 'Working' });
-});
+})
+.get('/:iduser',async (req,res)=>{
+  const {iduser}=req.params
+  const request=new sql.Request();
+  let data=await request.query(`select * from Clientes where idcliente=${iduser}`)
+  const client=data.recordsets[0][0]
+  data=await request.query(`select * from Contrato where idcliente=${iduser}`)
+  let contratos=[]
+  let contratosDetalle={}
+  data.recordsets[0].forEach(contrato=>{
+    contratos.push(contrato.NoContrato)
+    contratosDetalle[contrato.NoContrato]=contrato
+  })
+  res.status(200).json({client,contratos,contratosDetalle})
+
+})
 
 module.exports = router;

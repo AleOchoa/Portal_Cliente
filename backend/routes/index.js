cfb6b6c7861ab339e536b75ebce0d94f91b0d0af
Alejandra Ochoa <ale.ochoafi@gmail.com>
2020-03-31 11:52:06 -0600

    consulta base signup

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -28,7 +28,7 @@ router.get('/perfil/:iduser',async (req,res)=>{
                                   ).catch(err=>console.log(err))
   
   
-  const client=data.recordsets[0][0]
+  const cliente=data.recordsets[0][0]
   let contratos=[]
   let contratosDetalle={}
   let edoCuenta={}
@@ -40,7 +40,59 @@ router.get('/perfil/:iduser',async (req,res)=>{
   data.recordsets[2].forEach(registro=>{
     edoCuenta[registro.NoContrato][registro.FechaCorte.toISOString().substring(0,10)]=registro
   })
-  res.status(200).json({client,contratos,contratosDetalle,edoCuenta})
+  res.status(200).json({cliente,contratos,contratosDetalle,edoCuenta})
+})
+
+router.post('/signup',async (req,res)=>{
+  const {NoCliente,Nombre,Paterno,Materno,FechaNacimiento,Email,EmailConfirm,Password,PasswordConfirm}=req.body 
+  const request=new sql.Request();
+  //Se busca si hay un cliente con ese NoCliente, Nombre Completo y Fecha de Nacimiento
+  const data = await request.input("NoCliente",sql.Int,NoCliente)
+                  .input("NombreCompleto",sql.VarChar(200),Nombre+' '+Paterno+' '+Materno)
+                  .input("Fecha",sql.DateTime,FechaNacimiento)
+                  .query(`select * from Clientes where NoCliente= @NoCliente and NombreCliente=@NombreCompleto and FechaNacimiento=@Fecha`)
+                  .catch(err=>res.status(500).json({msg:'No se pudo agregar el usuario'})) 
+  //Si hay un cliente con esas características procedemos a crearlo
+  if (data.recordset[0]) {
+    //revisamos que no exista un usuario para ese cliente
+    const verifyUser= await request.input("IdCliente",sql.Int,data.recordset[0].IdCliente)
+                  .query('select * from Usuarios where IdCliente=@IdCliente').catch(err=>console.log(err))
+    if(verifyUser.rowsAffected[0]>0){
+      res.status(401).json({msg:"Usuario no generado. El cliente ya tiene una cuenta."})
+    }
+    else {
+      //Confirmamos que los emails sean iguales
+      if (Email===EmailConfirm) {
+        //Confirmamos que los passwords sean los mismos
+        if (Password===PasswordConfirm) {
+          //Procedemos a crear el usuario en bd
+          const newData = await request.input("email",sql.VarChar(100),Email)
+                    .input('password',sql.VarChar(20),Password)
+                    .input("IdPerfil",sql.Int,data.recordset[0].IdPerfil)
+                    .input("FechaDefault",sql.DateTime,'1900-01-01')
+                    .query(`insert into Usuarios (IdCliente,Email,Password,Bloqueo,
+          FecCambioPassword,PrimerAcceso,FecUltimoAcceso,IdPerfil
+          ) values (@IdCliente,@email,@password,0,@FechaDefault,1,@FechaDefault,@IdPerfil)`).catch(err=>console.log(err))
+          if(newData.rowsAffected[0]==1) {
+            res.status(200).json({user:data.recordset[0]})
+          }
+          else{
+            res.status(500).json({msg:"Algo salió mal"})
+          }
+        }
+        else {
+          res.status(400).json({msg:'Los passwords no coinciden'})
+        }  
+      }
+      else {
+        res.status(400).json({msg:'Los email no coinciden'})
+      }
+    }
+  }
+  else {
+    res.status(400).json({msg:'No existe el cliente'})
+  }
+  
 })
 
 module.exports = router;

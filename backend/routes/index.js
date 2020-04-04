001be900c4553f108b7173535b2f07f1e26d9746
Alejandra Ochoa <ale.ochoafi@gmail.com>
2020-04-03 21:34:19 -0600

    detalle movimientos correción para que se vean todos y no solo último

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -46,7 +46,7 @@ router.get('/perfil/:iduser',async (req,res)=>{
   const cliente=data.recordsets[0][0]
   let contratos=[]
   let contratosDetalle=[]
-  let edoCuenta=[]
+  let edoCuenta={}
   data.recordsets[1].forEach(contrato=>{
     contrato.show=false
     contratos.push(contrato.NoContrato[0])
@@ -60,7 +60,13 @@ router.get('/perfil/:iduser',async (req,res)=>{
     edoCuenta[registro.NoContrato][registro.VnFechaCorte.toISOString().substring(0,7)]['AvisoVencimiento']=registro
   })
   data.recordsets[4].forEach(registro=>{
-    edoCuenta[registro.NoContrato][registro.FechaCorte.toISOString().substring(0,7)]['DetalleMovimientos']=registro
+    edoCuenta[registro.NoContrato][registro.FechaCorte.toISOString().substring(0,7)]['DetalleMovimientos']=[]
+  })
+  data.recordsets[4].forEach(registro=>{
+    edoCuenta[registro.NoContrato][registro.FechaCorte.toISOString().substring(0,7)]['DetalleMovimientos'].push(registro)
+  })
+  contratosDetalle.forEach(contrato=>{
+    contrato['DetalleMovimientos']=edoCuenta[contrato.NoContrato[0]][contrato.FechaCorte.toISOString().substring(0,7)]['DetalleMovimientos']
   })
   const resumen=data.recordsets[5][0]
   res.status(200).json({cliente,contratos,contratosDetalle,edoCuenta,resumen})
